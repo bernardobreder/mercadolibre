fbcda1fb2c5a6ce1f9e90df0a5057d2bb8f05eaf
Bernardo Breder <bernardobreder@gmail.com>
2025-07-09 00:03:10 -0300

    .

--- frontend/src/components/ProductMain.tsx
+++ frontend/src/components/ProductMain.tsx
@@ -7,10 +7,10 @@ const ProductMain = ({ name, price }: Props) => {
   return (
     <div className="bg-white p-4 rounded shadow">
       <img
-        className="product-img transition-transform duration-300 ease-in-out hover:scale-105"
+        className="product-img transition-transform duration-300 ease-in-out hover:scale-105 mb-6"
         src="/public/product-1.webp"
-      ></img>
-      <h1 className="text-xl font-semibold mt-6 mb-2">{name}</h1>
+      />
+      <h1 className="text-xl font-semibold mb-2">{name}</h1>
       <p className="text-green-600 text-2xl font-bold">R$ {price}</p>
       <div class="flex gap-x-4">
         <button className="bg-blue-600 text-white px-4 py-2 rounded mt-4 w-full hover:bg-blue-700 flex-1">
